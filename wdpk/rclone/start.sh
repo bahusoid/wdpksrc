b1524e23da778aa175484f1aef5b5286e82c3385
Roman Artiukhin <bahusdrive@gmail.com>
2020-11-30 15:09:11 +0200

    fixes for rclone

--- wdpk/rclone/start.sh
+++ wdpk/rclone/start.sh
@@ -5,13 +5,13 @@ source ./env
 
 CONFIG=$(pwd)/rclone.config
 
-[ ! -f /usr/bin/rclone ] echo "rclone not found" >> /tmp/debug_apkg
+[ ! -f /usr/bin/rclone ] && echo "rclone not found" >> /tmp/debug_apkg
 
-[ -f /tmp/debug_apkg ] echo "APKG_DEBUG: starting Rclone" >> /tmp/debug_apkg
+[ -f /tmp/debug_apkg ] && echo "APKG_DEBUG: starting Rclone" >> /tmp/debug_apkg
 
 
 
 ADDRESS=$(sed -n '/ip/ {s/.*<ip>\(\S*\)<\/ip>/\1/p;q}' /etc/NAS_CFG/config.xml )
 
 # TODO: get user and pw from file
-rclone rcd --config ${CONFIG} --rc-web-gui-no-open-browser --rc-addr ${ADDRESS}:${PORT:-5574} --rc-user ${RCLONE_USER:-mycloud} --rc-pass ${RCLONE_PW:-mycloud}
+sudo rclone rcd --rc-web-gui-no-open-browser --rc-addr ${ADDRESS}:${PORT:-5574} --rc-user ${RCLONE_USER:-mycloud} --rc-pass ${RCLONE_PW:-mycloud}
