e8b59c57ac856abcf5bca55089904c6052bd6e5a
Roman Artiukhin <bahusdrive@gmail.com>
2020-11-30 10:13:31 +0200

    fixes for rclone

--- wdpk/rclone/remove.sh
+++ wdpk/rclone/remove.sh
@@ -5,16 +5,16 @@
 path=$1
 
 # create a backup of the config
-APKG_CONFIG="${path}/nzbget.conf"
-APKG_BACKUP_CONFIG="/mnt/HD/HD_a2/.systemfile/nzbget.conf"
-cp ${APKG_CONFIG} ${APKG_BACKUP_CONFIG}
+#APKG_CONFIG="${path}/nzbget.conf"
+#APKG_BACKUP_CONFIG="/mnt/HD/HD_a2/.systemfile/nzbget.conf"
+#cp ${APKG_CONFIG} ${APKG_BACKUP_CONFIG}
 
 rm -rf $path
 
 # remove bin
-rm -f /usr/bin/nzbget
+rm -f /usr/bin/rclone
 
 # remove lib
 
 # remove web
-rm -rf /var/www/nzbget
+rm -rf /var/www/rclone
--- wdpk/rclone/start.sh
+++ wdpk/rclone/start.sh
@@ -14,4 +14,4 @@ CONFIG=$(pwd)/rclone.config
 ADDRESS=$(sed -n '/ip/ {s/.*<ip>\(\S*\)<\/ip>/\1/p;q}' /etc/NAS_CFG/config.xml )
 
 # TODO: get user and pw from file
-rclone rcd --config ${CONFIG} --rc-web-gui --rc-addr ${ADDRESS}:${PORT:-5582} --rc-user ${RCLONE_USER:-mycloud} --rc-pass ${RCLONE_PW:-mycloud}
+rclone rcd --config ${CONFIG} --rc-web-gui-no-open-browser --rc-addr ${ADDRESS}:${PORT:-5574} --rc-user ${RCLONE_USER:-mycloud} --rc-pass ${RCLONE_PW:-mycloud}
